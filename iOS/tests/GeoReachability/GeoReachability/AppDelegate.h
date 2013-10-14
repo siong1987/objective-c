a80e2dda0c1ecc5bb8b7eb9be1ea990924bfcd42
Valentin Tuller <tullerval@gmail.com>
2013-10-15 00:03:01 +0300

    add json

--- iOS/tests/GeoReachability/GeoReachability/AppDelegate.h
+++ iOS/tests/GeoReachability/GeoReachability/AppDelegate.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, CLLocationManagerDelegate, PNDelegate> {
+	CLLocationManager *locationManager;
+	BOOL isInBackground;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
